c44e51337831fbd7e4e9e505a85ad0105c2a7655
AlexIerardi <alex.ierardi@CPT.local>
2022-09-23 14:42:56 +0200

    creazione progetto e view

---
+++ 5_Sito o applicativo/Dattilo_App/Dattilo/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dattilo.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private BaseViewModel _selectedViewModel = new ApprendimentoViewModel();
+
+        public BaseViewModel SelectedViewModel
+        {
+            get { return _selectedViewModel; }
+            set { _selectedViewModel = value; }
+        }
+
+    }
+}
